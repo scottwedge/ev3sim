9293a2c4aad70256941c7c30622855f3967ed0be
glipR <37640160+glipR@users.noreply.github.com>
2020-08-08 16:23:00 +1000

    Ensure screen is loaded before interactors.

--- ev3sim/simulation/loader.py
+++ ev3sim/simulation/loader.py
@@ -133,6 +133,7 @@ def runFromConfig(config, shared):
     from ev3sim.robot import initialise_bot, RobotInteractor
     from ev3sim.file_helper import find_abs
     sl = ScriptLoader(**config.get('loader', {}))
+    sl.startUp(**config.get('screen', {}))
     sl.setSharedData(shared)
     sl.active_scripts = []
     ev3sim.visual.utils.GLOBAL_COLOURS = config.get('colours', {})
@@ -145,7 +146,6 @@ def runFromConfig(config, shared):
         except Exception as exc:
             print(f"Failed to load interactor with the following options: {opt}. Got error: {exc}")
     if sl.active_scripts:
-        sl.startUp(**config.get('screen', {}))
         sl.loadElements(config.get('elements', []))
         for interactor in sl.active_scripts:
             if isinstance(interactor, RobotInteractor):
